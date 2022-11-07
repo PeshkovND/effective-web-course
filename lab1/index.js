3fe31d77484f57a0389a5f1c9429ec10644e7e48
PeshkovND <78641960+PeshkovND@users.noreply.github.com>
2022-11-07 22:36:54 +0600

    Feature/lab1 (#1)
    * feat: made header
    
    * feat: pricing and benefits sections
    
    * feat: footer
    
    * feat: adaptive to lower than 993px
    
    * feat: burger menu
    
    * refactor: css variables and group
    
    Co-authored-by: Nikita Peshkov <peshkov.nikita4716@gmail.com>

---
+++ lab1/index.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    const block = document.querySelector('.nav__links')
+    block.style.display = "flex"
+}
+
+function closeMenu() {
+    const block = document.querySelector('.nav__links')
+    block.style.display = "none"
+}
