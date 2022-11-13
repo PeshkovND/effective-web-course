7616703ad9e19dcd1f617552b2c26308c69f4f61
Nikita Peshkov <peshkov.nikita4716@gmail.com>
2022-11-13 21:44:20 +0600

    feat: add card and parsing

---
+++ lab3/src/components/card/index.tsx
@@ -0,0 +1,18 @@
+import styles from './card.module.css'
+
+interface CardProps {
+    name: string;
+    disc: string;
+    img: string
+}
+
+export const Card = ({name, disc, img}: CardProps) => {
+    return (
+        <div className={styles.cardContainer}>
+            <div className={styles.imgContainer}>
+                <img className={styles.img} src={img}></img>
+            </div>
+            <span className={styles.name}>{name}</span>
+            <p className={styles.disc}>{disc}</p>
+        </div>)
+}
