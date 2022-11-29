ade0c0f33ea559f8a29c01a9cd0ce2e9230666b3
Nikita Peshkov <peshkov.nikita4716@gmail.com>
2022-11-29 20:55:05 +0600

    feat: checkbox markup

--- lab3/src/components/Header/index.tsx
+++ lab3/src/components/Header/index.tsx
@@ -18,18 +18,25 @@ export const Header = observer((): ReactElement => {
       <div className={styles.logoContainer}>
         <img src="/marvel_logo.svg" className={styles.logo} alt="Marvel" />
       </div>
-      <nav className={styles.nav}>
-        <NavLink className={styles.navItem} to="/page/1">
-          Chartacters
-        </NavLink>
-        <NavLink className={styles.navItem} to="/comics/page/1">
-          Comics
-        </NavLink>
-        <NavLink className={styles.navItem} to="/series/page/1">
-          Series
-        </NavLink>
-        <input type="checkbox" onChange={() => themeStore.changeTheme(theme)} />
-      </nav>
+      <div className={styles.flexContainer}>
+        <nav className={styles.nav}>
+          <NavLink className={styles.navItem} to="/page/1">
+            Chartacters
+          </NavLink>
+          <NavLink className={styles.navItem} to="/comics/page/1">
+            Comics
+          </NavLink>
+          <NavLink className={styles.navItem} to="/series/page/1">
+            Series
+          </NavLink>
+          <div className={styles.checkboxWrapper} />
+        </nav>
+        <input
+          type="checkbox"
+          className={styles.switch_1}
+          onChange={() => themeStore.changeTheme(theme)}
+        />
+      </div>
     </header>
   );
 });
